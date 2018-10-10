5196ee284278500a8e2ca8d93896285f402084c3
bastian <bastianort@gmail.com>
2018-10-09 21:25:28 -0300

    cambios graficos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,3 +16,5 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( logo.png )
+Rails.application.config.assets.precompile += %w( *jpg )
